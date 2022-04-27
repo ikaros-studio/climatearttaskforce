1d727ca4fae6691021512a80b5fbcbfc35409f01
alexander-doudkin <alexander.doudkin@ecotrek.tech>
2022-04-27 22:52:00 +0200

    Fix a

--- components/ArtworkDescription.vue
+++ components/ArtworkDescription.vue
@@ -76,68 +76,71 @@
         </div>
       </div>
     </div>
-    <div
-      v-else-if="!artwork && !isMobile()"
-      variant="dark"
-      class="
-        p-4
-        overflow-auto
-        h-100
-        bg-white
-        z-50
-        px-2
-        border border-right
-        lh-1
-        text-dark
-        description-box
-        pt-5
-        mt-5
-      "
-    >
-      <p class="mt-5 catfont text-uppercase text-dark">
-        The climate art taskforce
-      </p>
-      <small class="fw-light"
-        >The climate art taskforce (<i>CAT</i>) is a global alliance of
-        interdisciplinary digital artists against climate change. Based on an
-        open source platform, <i>CAT</i> curates and publishes digital artworks
-        & publications making climate change accessible and approachable for a
-        non-expert audience. <i>CAT</i> invites digital practitioners to
-        contribute with their own relating digital works.
-
-        <br /><br />
-        With it’s open source approach, <i>CAT</i> aims to be inclusive towards
-        all kinds of artists and art formats relating to climate change,
-        everyone is welcome to contribute. Selected artworks and the platform
-        itself are constantly curated for external projects and campaigns.
-        <br />
-        <br />
-        The source code is available through
-        <b-link
-          href="https://github.com/ikarosstudios/climatearttaskforce"
-          target="_blank"
-          class="d-inline"
-          >www.github.com/ikarosstudios/climatearttaskforce</b-link
-        >
-        <br />
-        <br />All submissions are welcomed through
-        <b-link href="mailto:contact@ikaros.studio" target="_blank"
-          >contact@ikaros.studio</b-link
-        >
-        or direct repository pull requests.
-        <br />
-        <br />
-      </small>
-      <hr />
-      <b-button
-        variant="outline-dark"
-        size="sm"
-        href="https://www.artconnect.com/opportunity/ul_Y02MlSssJrrvE6q0vq"
-        target="_blank"
-        class="rounded-0 text-uppercase border-danger text-decoration-none"
-        >Be part of the taskforce</b-button
+    <client-only>
+      <div
+        v-if="!isMobile() && !artwork"
+        variant="dark"
+        class="
+          p-4
+          overflow-auto
+          h-100
+          bg-white
+          z-50
+          px-2
+          border border-right
+          lh-1
+          text-dark
+          description-box
+          pt-5
+          mt-5
+        "
       >
-    </div>
+        <p class="mt-5 catfont text-uppercase text-dark">
+          The climate art taskforce
+        </p>
+        <small class="fw-light"
+          >The climate art taskforce (<i>CAT</i>) is a global alliance of
+          interdisciplinary digital artists against climate change. Based on an
+          open source platform, <i>CAT</i> curates and publishes digital
+          artworks & publications making climate change accessible and
+          approachable for a non-expert audience. <i>CAT</i> invites digital
+          practitioners to contribute with their own relating digital works.
+
+          <br /><br />
+          With it’s open source approach, <i>CAT</i> aims to be inclusive
+          towards all kinds of artists and art formats relating to climate
+          change, everyone is welcome to contribute. Selected artworks and the
+          platform itself are constantly curated for external projects and
+          campaigns.
+          <br />
+          <br />
+          The source code is available through
+          <b-link
+            href="https://github.com/ikarosstudios/climatearttaskforce"
+            target="_blank"
+            class="d-inline"
+            >www.github.com/ikarosstudios/climatearttaskforce</b-link
+          >
+          <br />
+          <br />All submissions are welcomed through
+          <b-link href="mailto:contact@ikaros.studio" target="_blank"
+            >contact@ikaros.studio</b-link
+          >
+          or direct repository pull requests.
+          <br />
+          <br />
+        </small>
+        <hr />
+        <b-button
+          variant="outline-dark"
+          size="sm"
+          href="https://www.artconnect.com/opportunity/ul_Y02MlSssJrrvE6q0vq"
+          target="_blank"
+          class="rounded-0 text-uppercase border-danger text-decoration-none"
+          >Be part of the taskforce</b-button
+        >
+      </div>
+    </client-only>
   </div>
 </template>
 <script>
